c6b3d3bf8c6919fa79050706f57b173cdfe0fe13
unknown <bsuraj23@gmail.com>
2024-10-07 08:42:00 +0200

    start of project

---
+++ demo/src/test/java/com/example/BackendReusableApis/demo/BackendReusableApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendReusableApis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendReusableApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
