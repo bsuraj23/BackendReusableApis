cce7df7cac492e6b22734b87bd3fbc20eab53197
unknown <bsuraj23@gmail.com>
2024-10-08 10:30:13 +0200

    adding new parallel execution

---
+++ demo/src/main/java/com/example/BackendReusableApis/demo/otherParallelPipeLine.java
@@ -0,0 +1,13 @@
+package com.example.BackendReusableApis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherParallelPipeline {
+
+	public static void main(String[] args) {
+	//	SpringApplication.run(BackendReusableApisApplication.class, args);
+	}
+
+}
