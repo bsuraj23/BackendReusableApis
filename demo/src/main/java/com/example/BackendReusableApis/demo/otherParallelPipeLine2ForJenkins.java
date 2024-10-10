59283f4599d2b0ed9f8f159167dd9f35dfa0b4e8
unknown <bsuraj23@gmail.com>
2024-10-10 11:10:05 +0200

    adding jenkins code

---
+++ demo/src/main/java/com/example/BackendReusableApis/demo/otherParallelPipeLine2ForJenkins.java
@@ -0,0 +1,6 @@
+package com.example.BackendReusableApis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//jenkins Support code here
