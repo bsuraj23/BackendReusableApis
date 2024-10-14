591aa5ac8ef1bb1961af997bed92c3e1a03ad543
unknown <bsuraj23@gmail.com>
2024-10-14 08:05:47 +0200

    adding new depencies for other framwework

--- demo/src/main/java/com/example/BackendReusableApis/demo/otherParallelPipeLine2ForJenkins.java
+++ demo/src/main/java/com/example/BackendReusableApis/demo/otherParallelPipeLine2ForJenkins.java
@@ -4,3 +4,4 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //jenkins Support code here
+//Support for Docker giving erorr so removing the otehr depencies 
