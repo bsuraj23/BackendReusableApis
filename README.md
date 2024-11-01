b207063296ac222f9720551224becdf0a39afcfb
unknown <bsuraj23@gmail.com>
2024-11-01 23:19:28 +0100

    adding rest templates

--- README.md
+++
@@ -1,2 +0,0 @@
-# BackendReusableApis
-This project will help Java developers to set the initial reusable code that can be used in any project 
--- demo/src/main/java/com/example/BackendReusableApis/demo/otherParallelPipeLine2ForJenkins.java
+++
@@ -1,7 +0,0 @@
-package com.example.BackendReusableApis.demo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-//jenkins Support code here
-//Support for Docker giving erorr so removing the otehr depencies 
